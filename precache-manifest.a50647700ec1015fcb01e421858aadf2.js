e5cf939e2d6a98bdafbbccf071d7f960e3574ca5
Sabir Hussain <sabir5758665@gmail.com>
2023-07-15 05:12:20 -0400

    Updates

---
+++ precache-manifest.a50647700ec1015fcb01e421858aadf2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ee8c3337115478dc8188ef3aaf07102",
+    "url": "/KeeperApp/index.html"
+  },
+  {
+    "revision": "a4d931c63f47251f68ff",
+    "url": "/KeeperApp/static/js/2.260104d4.chunk.js"
+  },
+  {
+    "revision": "7c0ee208789c53628f41",
+    "url": "/KeeperApp/static/js/main.569e6eb9.chunk.js"
+  },
+  {
+    "revision": "ad81aa77cc91800000fc",
+    "url": "/KeeperApp/static/js/runtime-main.854af2fe.js"
+  }
+]);
